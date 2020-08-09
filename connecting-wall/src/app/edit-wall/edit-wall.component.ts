20fdda52950d2c36d3de0652e4b779036b228f1c
fcuic <filipcuic.nasice@gmail.com>
2020-08-09 12:12:54 +0200

    Updating wall completed

--- connecting-wall/src/app/edit-wall/edit-wall.component.ts
+++ connecting-wall/src/app/edit-wall/edit-wall.component.ts
@@ -14,68 +14,171 @@ export class EditWallComponent implements OnInit {
 
   readonly BaseURI = 'http://localhost:57392/api';
   private header=new HttpHeaders({'content-type': 'application/json'});
+  wallID:string;
+  wallDetails:any;
+  //#region date updated conversion to C# format
+  
+  
+
 
 
   constructor(public service:WallService,private userService:UserService,private toastr:ToastrService, private http:HttpClient) { }
 
   ngOnInit(): void 
   {
-   /* this.service.()
-      .subscribe(
-        res=>{
-          this.userDetails=res;
-          console.log(this.userDetails);
-        },
-        err=>{
-          console.log(err);
-        }
-      )*/
+   /*this.service.getWallById(this.service.formModel.value.wallID).subscribe(
+    res=>{
+      this.WallDetails=res;
+      //console.log(this.WallDetails.groupATerms[0].termName);
+      //console.log(this.WallDetails.groupBTerms[0].termName);
+      console.log(this.WallDetails);
+    }
+  );*/
+  this.GetWallData();
+  //this.service.getWallById()
   }
+
+  /*public dateToString(date:Date)
+  {
+    let day=date.getDate();
+    let month=date.getMonth();
+    let year=date.getFullYear();
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+    let miliseconds=date.getMilliseconds();
+
+    let dateStringInCFormat=year+'-0'+month+'-0'+day+'T'+hours+':'+minutes+':'+seconds+"."+miliseconds;
+    return dateStringInCFormat;
+  }*/
+  GetWallData()
+  {
+    
+  this.wallID=this.service.formModel.value.wallID;
+  if(this.wallID==null){
+    this.toastr.warning('Could not get wall ID','Please close and open this window again');
+  }
+  console.log(this.wallID);
+  this.service.getWallById(this.wallID).subscribe(//zato sto vraca observable bez subscribea
+    res=>{
+      this.wallDetails=res;
+      console.log(this.wallDetails);
+      /*console.log(this.wallDetails.userID);
+      console.log(this.wallDetails.groupAConnections[0].connectionId);
+      console.log(this.wallDetails.groupBConnections[0].connectionId);
+      console.log(this.wallDetails.groupCConnections[0].connectionId);
+      console.log(this.wallDetails.groupDConnections[0].connectionId);*/
+    });
+  }
+  
   UpdateWall()
   {
     var body={
       wallID:this.service.formModel.value.wallID,
+      userID:this.wallDetails.userID,
       wallName:this.service.formModel.value.wallName,
+      dateCreated:this.wallDetails.dateCreated,
      groupATerms:[
-        {termName: this.service.formModel.get("GroupATerms").get("GroupATerm1").value},
-        {termName: this.service.formModel.get("GroupATerms").get("GroupATerm2").value},
-        {termName: this.service.formModel.get("GroupATerms").get("GroupATerm3").value},
-        {termName: this.service.formModel.get("GroupATerms").get("GroupATerm4").value}]
+        {
+          termID:this.wallDetails.groupATerms[0].termID,
+          termName: this.service.formModel.get("GroupATerms").get("GroupATerm1").value
+        },
+        {
+          termID:this.wallDetails.groupATerms[1].termID,
+          termName: this.service.formModel.get("GroupATerms").get("GroupATerm2").value
+        },
+        {
+          termID:this.wallDetails.groupATerms[2].termID,
+          termName: this.service.formModel.get("GroupATerms").get("GroupATerm3").value
+        },
+        {
+          termID:this.wallDetails.groupATerms[3].termID,
+          termName: this.service.formModel.get("GroupATerms").get("GroupATerm4").value
+        }]
       ,
       groupBTerms:[
-        {termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm1").value},
-        {termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm2").value},
-        {termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm3").value},
-        {termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm4").value}]
+        {
+          termID:this.wallDetails.groupBTerms[0].termID,
+          termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm1").value
+        },
+        {
+          termID:this.wallDetails.groupBTerms[1].termID,
+          termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm2").value
+        },
+        {
+          termID:this.wallDetails.groupBTerms[2].termID,
+          termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm3").value
+        },
+        {
+          termID:this.wallDetails.groupBTerms[3].termID,
+          termName: this.service.formModel.get("GroupBTerms").get("GroupBTerm4").value
+        }]
       ,
       groupCTerms:[
-        {termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm1").value},
-        {termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm2").value},
-        {termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm3").value},
-        {termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm4").value}]
+        {
+          termID:this.wallDetails.groupCTerms[0].termID,
+          termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm1").value
+        },
+        {
+          termID:this.wallDetails.groupCTerms[1].termID,
+          termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm2").value
+        },
+        {
+          termID:this.wallDetails.groupCTerms[2].termID,
+          termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm3").value
+        },
+        {
+          termID:this.wallDetails.groupCTerms[3].termID,
+          termName: this.service.formModel.get("GroupCTerms").get("GroupCTerm4").value
+        }]
       ,
       groupDTerms:[
-        {termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm1").value},
-        {termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm2").value},
-        {termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm3").value},
-        {termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm4").value}]
+        {
+          termID:this.wallDetails.groupDTerms[0].termID,
+          termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm1").value
+        },
+        {
+          termID:this.wallDetails.groupDTerms[1].termID,
+          termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm2").value
+        },
+        {
+          termID:this.wallDetails.groupDTerms[2].termID,
+          termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm3").value
+        },
+        {
+          termID:this.wallDetails.groupDTerms[3].termID,
+          termName: this.service.formModel.get("GroupDTerms").get("GroupDTerm4").value
+        }]
       ,
       groupAConnections:[
-        {connectionName:this.service.formModel.value.GroupAConnections}
+        {
+          connectionId:this.wallDetails.groupAConnections[0].connectionId,
+          connectionName:this.service.formModel.value.GroupAConnections
+        }
       ]
       ,
       groupBConnections:[
-        {connectionName:this.service.formModel.value.GroupBConnections}
+        {
+          connectionId:this.wallDetails.groupBConnections[0].connectionId,
+          connectionName:this.service.formModel.value.GroupBConnections
+        }
       ]
       ,
       groupCConnections:[
-        {connectionName:this.service.formModel.value.GroupCConnections}
+        {
+          connectionId:this.wallDetails.groupCConnections[0].connectionId,
+          connectionName:this.service.formModel.value.GroupCConnections
+        }
       ]
       ,
       groupDConnections:[
-        {connectionName:this.service.formModel.value.GroupDConnections}
+        {
+          connectionId:this.wallDetails.groupDConnections[0].connectionId,
+          connectionName:this.service.formModel.value.GroupDConnections
+        }
       ]
   }
+    //console.log(body.wallID);
     console.log(body);
     return this.http.put(this.BaseURI+'/Wall/'+body.wallID,body,{headers:this.header});
     
